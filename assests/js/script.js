e29f98b13182f5640c5412b241c978d40ca031cb
Deniks71 <101110923+Deniks71@users.noreply.github.com>
2023-04-08 00:15:13 -0300

    Add files via upload

---
+++ assests/js/script.js
@@ -0,0 +1,17 @@
+let total;
+document.getElementById('calculate').addEventListener('click', function(){
+    let value = document.getElementById('value').value;
+    let fee = (document.getElementById('fee').value) / 100;
+    let time = document.getElementById('time').value;
+    total = value * (1 + fee)**time;
+
+    document.getElementById('total').innerHTML = ("R$ " + total.toFixed(2).replace('.', ','));
+});
+
+document.getElementById('clean').addEventListener('click', function(){
+    total = 0
+    document.getElementById('total').innerHTML = ("R$ " + total.toFixed(2).replace('.', ','));
+    
+    
+
+})
